c4875e5e85e1c68704c120b63d1663441b11e6f8
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-23 02:53:01 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 OWNER = ["K_O_W_4"]
 OWNER_NAME = "𓏺 َِ𝗕َِ𝗲َِ𝗟َِ𝗮َِ𝗟 َِ𝗻َِ𝗢َِ𝘁 َِ𝗦َِ𝗵َِ𝗔َِ𝗱َِ𝗢َِ𝘄 ."
-BOT_TOKEN = getenv("BOT_TOKEN")
+BOT_TOKEN = getenv("6477407510:AAH7qtd4tabFhx7o5wzwsB-mwUd_TqIozFk")
 DATABASE = getenv("MONGO_DB_URI", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
