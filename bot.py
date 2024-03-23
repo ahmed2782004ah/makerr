e5d97e2de9f5f989899a0f82621b43de303373ed
ahmed2782004ah <163062689+ahmed2782004ah@users.noreply.github.com>
2024-03-23 02:56:29 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,16 +6,16 @@ from pyromod import listen
 
 bot = Client(
     "mo",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
+    api_id="25336226",
+    api_hash="fc6670f0d2070c0a6defb9c25b92c384",
+    bot_token="6477407510:AAH7qtd4tabFhx7o5wzwsB-mwUd_TqIozFk",
     plugins=dict(root="Maker")
     )
 
 async def start_bot():
     print("[INFO]: STARTING BOT CLIENT")
     await bot.start()
-    A_q_lp = "E_Z_9"
+    A_q_lp = "IIUll_l"
     await bot.send_message(A_q_lp, "** اشتغلت يولبولبول . **")
     print("[INFO]:   ")
     await idle()
